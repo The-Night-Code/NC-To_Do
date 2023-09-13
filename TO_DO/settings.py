4218fc25d1d1c8020fefe81fe6ad05632d638dd0
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2023-09-13 23:18:35 +0100

    v0

--- TO_DO/settings.py
+++ TO_DO/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2kdzvcp#lc!ns1elp&-ayv+cmlfp9vjy(k9@kuxllfqlbt(s+l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- TO_DO/urls.py
+++ TO_DO/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from ToDo_app.views import Home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', Home)
 ]
